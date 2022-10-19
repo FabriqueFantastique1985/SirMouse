8ad37f30fa5aa44e7197a123393465f03a00f38f
Koen <Koen.laureys@fabriqueFantastique.be>
2022-10-19 16:43:36 +0200

    Encounterable created, AudioController aditions, Touchable start

--- Assets/Scripts/UnityCore/Audio/TestAudio.cs
+++ Assets/Scripts/UnityCore/Audio/TestAudio.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace UnityCore
@@ -9,50 +10,96 @@ namespace UnityCore
         {
             public AudioController AudioControl;
 
+            public List<AudioElement> AudioElementals = new List<AudioElement>();
+            public List<AudioElement> AudioOST = new List<AudioElement>();
+            public List<AudioElement> AudioUI = new List<AudioElement>();
+
+            private void Start()
+            {
+                foreach (var em in AudioElementals)
+                {
+                    if (em != null)
+                    {
+                        AudioControl.AddAudioElement(em, 2);
+                    }                   
+                }
+                foreach (var em in AudioOST)
+                {
+                    if (em != null)
+                    {
+                        AudioControl.AddAudioElement(em, 0);
+                    }                    
+                }
+                foreach (var em in AudioUI)
+                {
+                    if (em != null)
+                    {
+                        AudioControl.AddAudioElement(em, 1);
+                    }                   
+                }
+            }
+
 
             private void Update()
             {
+                /// world sounds testing ///
                 if (Input.GetKeyUp(KeyCode.T))
                 {
-                    AudioControl.PlayAudio(AudioType.OST, true, 1.0f);
+                    AudioControl.PlayAudio(AudioElementals[0].Clip, AudioElementals[0].Type);
                 }
                 if (Input.GetKeyUp(KeyCode.G))
                 {
-                    AudioControl.StopAudio(AudioType.OST, true);
+                    AudioControl.StopAudio(AudioElementals[0].Type);
                 }
                 if (Input.GetKeyUp(KeyCode.B))
                 {
-                    AudioControl.RestartAudio(AudioType.OST, true);
+                    AudioControl.RestartAudio(AudioElementals[0].Type);
                 }
-
-
 
                 if (Input.GetKeyUp(KeyCode.Y))
                 {
-                    AudioControl.PlayAudio(AudioType.SFX_World);
-                }
-                if (Input.GetKeyUp(KeyCode.H))
-                {
-                    AudioControl.StopAudio(AudioType.SFX_World);
-                }
-                if (Input.GetKeyUp(KeyCode.N))
-                {
-                    AudioControl.RestartAudio(AudioType.SFX_World);
+                    AudioControl.PlayAudio(AudioElementals[1].Clip, AudioElementals[1].Type);
                 }
 
 
-
+                /// OST testing ///
                 if (Input.GetKeyUp(KeyCode.U))
                 {
-                    AudioControl.PlayAudio(AudioType.SFX_UI);
+                    AudioControl.PlayAudio(AudioOST[0].Clip, AudioOST[0].Type);
                 }
                 if (Input.GetKeyUp(KeyCode.J))
                 {
-                    AudioControl.StopAudio(AudioType.SFX_UI);
+                    AudioControl.StopAudio(AudioOST[0].Type);
                 }
                 if (Input.GetKeyUp(KeyCode.M))
                 {
-                    AudioControl.RestartAudio(AudioType.SFX_UI);
+                    AudioControl.RestartAudio(AudioOST[0].Type);
+                }
+
+                if (Input.GetKeyUp(KeyCode.I))
+                {
+                    AudioControl.PlayAudio(AudioOST[1].Clip, AudioOST[1].Type);
+                }
+
+
+
+                /// UI testing ///
+                if (Input.GetKeyUp(KeyCode.E))
+                {
+                    AudioControl.PlayAudio(AudioUI[0].Clip, AudioUI[0].Type);
+                }
+                if (Input.GetKeyUp(KeyCode.D))
+                {
+                    AudioControl.StopAudio(AudioUI[0].Type);
+                }
+                if (Input.GetKeyUp(KeyCode.C))
+                {
+                    AudioControl.RestartAudio(AudioUI[0].Type);
+                }
+
+                if (Input.GetKeyUp(KeyCode.R))
+                {
+                    AudioControl.PlayAudio(AudioUI[1].Clip, AudioUI[1].Type);
                 }
             }
         }
