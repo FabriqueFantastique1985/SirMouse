7edce11148d62403dcded19de6c821fecba3fcd3
Koen <Koen.laureys@fabriqueFantastique.be>
2023-03-03 15:25:00 +0100

    tweak audio

--- Assets/Scripts/UnityCore/Menus/Buttons/NEW/ButtonBackpack.cs
+++ Assets/Scripts/UnityCore/Menus/Buttons/NEW/ButtonBackpack.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityCore.Menus;
+using UnityCore.Audio;
 
 public class ButtonBackpack : ButtonPaging
 {
@@ -13,6 +14,8 @@ public class ButtonBackpack : ButtonPaging
 
             _pageInstance.OpenBagImage(false);
             _pageInstance.OpenClosetImage(false);
+
+            //AudioController.Instance.TurnDownVolumeForOSTAndWorld(false);
 
             // sleeping allowed
             GameManager.Instance.Player.Character.SetBoolSleeping(false);
@@ -28,6 +31,8 @@ public class ButtonBackpack : ButtonPaging
             SkinsMouseController.Instance.ClosetWrapInsideCamera.gameObject.SetActive(false);
 
             _pageInstance.TurnAllPagesOffExcept(_turnThisPage);
+
+            //AudioController.Instance.TurnDownVolumeForOSTAndWorld();
 
             _pageInstance.OpenBagImage(true);
             _pageInstance.OpenClosetImage(false);
