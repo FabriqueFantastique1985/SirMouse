d21df6d9738ae36ebf655b383a3db9dd6839aa17
Rica-FF <99659612+Rica-FF@users.noreply.github.com>
2023-03-15 11:25:17 +0100

    Added lots of skinpieces to SirMouse Rig Variant

--- Assets/Scripts/Closet/Type_Skin.cs
+++ Assets/Scripts/Closet/Type_Skin.cs
@@ -12,5 +12,58 @@ public enum Type_Skin
     Chicken = 1,
     Ostrich = 2,
     Pyjama = 3,
-    blacksmith = 4
+    blacksmith = 5,
+    MrWitch = 6,
+    ArmorGold = 9,
+    ArmorGoldAlt = 10,
+    Jester = 11,
+    Prince = 12,
+
+    // Random Hats
+    ChefDefault = 7,
+    ChefGold = 8,
+    Strainer = 13,
+    ArmorHelmet = 14,
+    ArmorHelmet_01 = 15,
+    PrincessHat_01 = 16,
+    RandomHat_01 = 17,
+    RandomHat_02 = 18,
+    RobinHoodHat = 19,
+
+    // Random Heads
+    Scarf_01 = 20,
+    Carrot = 21,
+    RedNose = 22,
+
+    // Random Arms
+    OvenMittens = 23,
+
+    // Random Feet
+    Slippers = 24,
+
+    // Random Swords
+    Excalibur = 25,
+    WoodenSword = 26,
+    SirKnightSword = 27,
+    SmartSword = 28,
+    Mace = 29,
+    Spear = 30,
+    BigAxe = 31,
+    SmallAxe = 32,
+    Bow = 33,
+    Sword_01 = 34,
+    Sword_02 = 35,
+    MorningStar = 36,
+    FondueFork = 37,
+    RibbonStick = 38,
+    ButterflyNet = 39,
+
+    // Random Shields
+    Mole = 40,
+    WoodenShield = 41,
+    Shield_01 = 42,
+    Shield_02 = 43,
+    Shield_03 = 44,
+    Shield_04 = 45
+
 }
