68861576d0bda76ad658ffb756ca04c6842885aa
Rica-FF <99659612+Rica-FF@users.noreply.github.com>
2023-03-16 11:10:00 +0100

    Slippers interactable update

---
+++ Assets/Scripts/Interactables/Interactable_SeparatedSprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_SeparatedSprite : Interactable
+{
+    [Header("Separated Sprite")] [SerializeField] private Transform _separatedSprite;
+
+    protected override void OnInteractBalloonClicked(Balloon sender, Player player)
+    {
+        base.OnInteractBalloonClicked(sender, player);
+
+        Destroy(_separatedSprite.gameObject);
+    }
+}
