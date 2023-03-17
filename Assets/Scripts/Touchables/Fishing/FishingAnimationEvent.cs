a8c8530854604d121ed67622b091abd4e4607015
Sari Janssen <sari.janssen@fabriquefantastique.be>
2023-03-17 14:44:04 +0100

    fixed fishing rod animation and speed

---
+++ Assets/Scripts/Touchables/Fishing/FishingAnimationEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingAnimationEvent : MonoBehaviour
+{
+    [SerializeField] private BackAndForth_Continous _fishingHook;
+    [SerializeField] private Touch_Event _touchEvent;
+    [SerializeField] private Touchable _touchable;
+
+    private void Start()
+    {
+        _touchable.OneTimeUse = true;
+        _fishingHook.OnReachedStart += EnableAnimation;
+        _touchEvent.OnPropTouched += DisableAnimation;
+    }
+
+    private void OnDestroy()
+    {
+        _fishingHook.OnReachedStart -= EnableAnimation;
+        _touchEvent.OnPropTouched -= DisableAnimation;
+    }
+
+    private void EnableAnimation()
+    {
+        _touchable.UsedSuccesfully = false;
+    }
+    private void DisableAnimation()
+    {
+        _touchable.UsedSuccesfully = true;
+    }
+}
