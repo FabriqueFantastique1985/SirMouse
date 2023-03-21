2f7a6ec229a3beb71b367f06c377aa4d7ac90537
Koen <Koen.laureys@fabriqueFantastique.be>
2023-03-21 10:04:35 +0100

    Added leaves to forest

--- Assets/Scripts/Interactables/Interactions/InteractionNeedyPickupDelivered.cs
+++ Assets/Scripts/Interactables/Interactions/InteractionNeedyPickupDelivered.cs
@@ -16,25 +16,32 @@ public class InteractionNeedyPickupDelivered : Interaction
 
         // for all items in players backpack, check if they correspond with a type I need
         List<ButtonPickupBackpack> buttonPickupsToRemove = new List<ButtonPickupBackpack>();
+
+        //List<Type_Pickup> tempList = new List<Type_Pickup>();
+        //for (int i = 0; i < _myInteractable.WantedPickups.Count; i++)
+        //{
+        //    tempList.Add(_myInteractable.WantedPickups[i]);
+        //}
+
         for (int i = 0; i < _myInteractable.WantedPickups.Count; i++)
         {
-            Debug.Log("test 0");
+            //Debug.Log("test 0");
             for (int j = 0; j < BackpackController.ItemButtonsInBackpack.Count; j++)
             {
-                Debug.Log("test 1");
-                if (BackpackController.ItemButtonsInBackpack[j].MyPickupType == _myInteractable.WantedPickups[i])
+                //Debug.Log("test 1");
+                if (BackpackController.ItemButtonsInBackpack[j].MyPickupType == tempList[i])
                 {
-                    Debug.Log("test 2");
+                    //Debug.Log("test 2");
                     // if this has not yet been added to my list (dont want to add the exact same pickup 3 times)
                     if (buttonPickupsToRemove.Contains(BackpackController.ItemButtonsInBackpack[j]) == false)
                     {
-                        Debug.Log("giving 1 item BAG");
+                        //Debug.Log("giving 1 item BAG");
 
                         var buttonOfInterest = BackpackController.ItemButtonsInBackpack[j];
                         buttonPickupsToRemove.Add(buttonOfInterest);
 
                         //_myInteractable.HeldPickups.Add(buttonOfInterest.MyPickupType);
-                        _myInteractable.WantedPickups.Remove(_myInteractable.WantedPickups[i]); // should not be removing from the list it's looping in
+                        _myInteractable.WantedPickups.Remove(_myInteractable.WantedPickups[i]); // should not be removing from the list it's looping in...
 
                         // update needy balloon
                         _myInteractable.NeedyBalloon.UpdateOneRequiredNeedyPickup(BackpackController.ItemButtonsInBackpack[j].MyPickupType);
