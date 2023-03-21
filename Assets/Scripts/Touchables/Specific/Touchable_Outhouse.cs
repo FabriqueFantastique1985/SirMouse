efc910355ddc57ff56d2eff25f3e249a22b7de59
Koen <Koen.laureys@fabriqueFantastique.be>
2023-03-21 12:27:14 +0100

    Outhouse reward + fix ovenmittens

--- Assets/Scripts/Touchables/Specific/Touchable_Outhouse.cs
+++ Assets/Scripts/Touchables/Specific/Touchable_Outhouse.cs
@@ -10,6 +10,19 @@ public class Touchable_Outhouse : Touchable
     [SerializeField]
     private List<Sprite> _possibleBottleSprites = new List<Sprite>();
 
+    private bool _gaveReward;  // save data here
+    private int _indexAnimation = 0;
+
+    [SerializeField]
+    private List<SkinPieceElement> _skinRewards;
+
+
+    protected override void Start()
+    {
+        base.Start();
+
+        _indexAnimation = 0;
+    }
 
     // animation event
     public void RandomizeBottleSprite()
@@ -17,5 +30,37 @@ public class Touchable_Outhouse : Touchable
         var random = Random.Range(0, _possibleBottleSprites.Count - 1);
 
         _bottleSprite.sprite = _possibleBottleSprites[random];
+    }
+
+
+
+    protected override void TriggerAnimation()
+    {
+        if (Animator != null)
+        {
+            Animator.SetTrigger("Activate");
+            _indexAnimation += 1;
+
+            if (_indexAnimation > 2)
+            {
+                _indexAnimation = 0;
+
+                if (_gaveReward == false)
+                {
+                    _gaveReward = true;
+
+                    RewardController.Instance.StartCoroutine(GiveReward());
+                }  
+            }
+        }
+    }
+
+
+
+    private IEnumerator GiveReward()
+    {
+        yield return new WaitForSeconds(0.6f); // short delay 
+
+        RewardController.Instance.GiveReward(_skinRewards);
     }
 }
