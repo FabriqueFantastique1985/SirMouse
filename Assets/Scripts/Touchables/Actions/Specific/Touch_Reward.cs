378cbcc9df8bcbd71d9bd132256e5319e44f7797
Sari Janssen <sari.janssen@fabriquefantastique.be>
2023-03-27 17:32:58 +0200

    Adding broom reward and ceiling swing animation

---
+++ Assets/Scripts/Touchables/Actions/Specific/Touch_Reward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Touch_Reward : Touch_Action
+{
+    [Header("Reward")]
+    [SerializeField] private List<SkinPieceElement> _skinsToReward;
+
+    private bool _hasCollected = false;
+
+    public override void Act()
+    {
+        if (_hasCollected)
+        {
+            return;
+        }
+
+        if (_skinsToReward.Count > 0)
+        {
+            RewardController.Instance.GiveReward(_skinsToReward);
+            _hasCollected = true;
+        }
+    }
+}
