d774698689b6f77ccba8e06d8ab5dd077174e966
Sari Janssen <sari.janssen@fabriquefantastique.be>
2023-04-12 11:03:03 +0200

    Added mirror interactable to cave bedroom

--- Assets/Scripts/Interactables/Interactions/InteractionGetReward.cs
+++ Assets/Scripts/Interactables/Interactions/InteractionGetReward.cs
@@ -5,12 +5,16 @@ using UnityEngine.Serialization;
 
 public class InteractionGetReward : Interaction
 {
+    public delegate void InteractionGetRewardDelegate();
+    public event InteractionGetRewardDelegate GetReward;
+
     [SerializeField] private List<SkinPieceElement> _mySkinPieceElements = new List<SkinPieceElement>();
 
     protected override void SpecificAction(Player player)
     {
         base.SpecificAction(player);
         RewardController.Instance.GiveReward(_mySkinPieceElements);
+        GetReward?.Invoke();
     }
 
 }
