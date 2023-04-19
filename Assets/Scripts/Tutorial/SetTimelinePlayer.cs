bc19df44091787871e9eb76e6fa4e8a50766e0a4
Sari Janssen <sari.janssen@fabriquefantastique.be>
2023-04-19 15:05:32 +0200

    Added cutscene prefab to be added to forest scene later

---
+++ Assets/Scripts/Tutorial/SetTimelinePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SetTimelinePlayer : MonoBehaviour
+{
+    [SerializeField] private int _timelineIndex = -1;
+    private SetTimelineReference _timelineReference;
+
+    private void Start()
+    {
+        _timelineReference = GetComponent<SetTimelineReference>();
+        _timelineReference.OnTimelineStarted += SetTimelineObjects;
+    }
+
+    public void SetTimelineObjects(PlayableDirector director)
+    {
+        _timelineReference.OnTimelineStarted -= SetTimelineObjects;
+
+        GameObject player = GameManager.Instance.Player.gameObject;
+
+        if (_timelineIndex >= 0)
+        {
+            _timelineReference.SetTimelineObjectAt(player, _timelineIndex);
+            return;
+        }
+
+        for (int i = 0; i < _timelineReference.GetTimelineCount(); i++)
+        {
+            _timelineReference.SetTimelineObjectAt(player, i);
+        }
+    }
+
+}
