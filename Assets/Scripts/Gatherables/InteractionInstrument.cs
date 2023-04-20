2425d80a233b7d4fb7ee96a5cdc70c8a66dc0b7c
Sari Janssen <sari.janssen@fabriquefantastique.be>
2023-04-20 16:40:46 +0200

    Possible fix for instrument balloon bug

--- Assets/Scripts/Gatherables/InteractionInstrument.cs
+++ Assets/Scripts/Gatherables/InteractionInstrument.cs
@@ -7,10 +7,7 @@ public class InteractionInstrument : Interaction
     protected override void SpecificAction(Player player)
     {
         base.SpecificAction(player);
-
-        Debug.Log("INTERACTED WOOHOO");
     }
-
 
     // method to hide any objects that shouldn't be there anymore (like a chest)
     public virtual void HideInteraction()
