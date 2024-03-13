2d78851aab25badbb407dca69cb6dd1c59061360
Rica-FF <ricardo.gomes@fabriquefantastique.be>
2024-03-13 08:19:31 +0100

    Update loadingscreens

--- Assets/Scripts/UI/CanvasScalerManager.cs
+++ Assets/Scripts/UI/CanvasScalerManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class CanvasScalerManager : MonoBehaviour
 {
+    [Header("Canvas")]
     [SerializeField]
     private CanvasScaler _canvasScaler;
     private float _canvasRatio;
@@ -12,6 +13,16 @@ public class CanvasScalerManager : MonoBehaviour
     private int _minWidth = 1050;
     [SerializeField]
     private int _maxWidth = 1340;
+
+    [Header("Rect Transform")]
+    [SerializeField]
+    private RectTransform _rectTransform;
+    [SerializeField]
+    private float _minScale = 1;
+    [SerializeField]
+    private float _maxScale = 1.6f;
+
+    [Header("Settings")]
     [SerializeField]
     private float _minRatio = 1.3f;
     [SerializeField]
@@ -36,6 +47,16 @@ public class CanvasScalerManager : MonoBehaviour
     {
         _canvasRatio = (float)Screen.width / Screen.height;
         float lerpValue = Mathf.Clamp(_canvasRatio, _minRatio, _maxRatio) - _minRatio;
-        _canvasScaler.referenceResolution = new Vector2(Mathf.Lerp(_minWidth, _maxWidth, lerpValue), _canvasScaler.referenceResolution.y);
+
+        if (_canvasScaler != null)
+        {
+            _canvasScaler.referenceResolution = new Vector2(Mathf.Lerp(_minWidth, _maxWidth, lerpValue), _canvasScaler.referenceResolution.y);
+        }
+
+        if (_rectTransform != null)
+        {
+            float scaleValue = Mathf.Lerp(_minScale, _maxScale, lerpValue);
+            _rectTransform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+        }
     }
 }
